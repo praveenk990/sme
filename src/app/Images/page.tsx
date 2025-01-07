ed4e95981c419fc6620626aa92db2bec4d4a3f7c
praveenk990 <pk9740273832@gmail.com>
2025-01-08 03:36:34 +0530

    Add ParallaxScroll component with 4-grid layout and responsive design2

--- src/app/Images/page.tsx
+++ src/app/Images/page.tsx
@@ -2,39 +2,32 @@
 import { ParallaxScrollSecond } from "./ui/parallax-scroll-2";
 import { useEffect, useState } from "react";
 
-export function Image() {
+const ImagesPage = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Check if the screen width is less than 768px (mobile view)
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
-    // Initial check
     handleResize();
-
-    // Add event listener for resize
     window.addEventListener("resize", handleResize);
 
-    // Cleanup event listener
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <div className="h-[40rem] w-full bg-gray-500 bg-grid-white/[0.2] relative flex items-center justify-center">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center "></div>
+    <div className="h-[50rem] w-full bg-gray-500 bg-grid-white/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-      {/* Render all images with a smaller size and scrollable vertically on mobile, else use parallax scroll */}
       {isMobile ? (
-        <div className="overflow-y-auto flex flex-col pt-20 pb-8 space-y-4 h-full w-full px-4">
+        <div className="overflow-y-auto flex flex-col py-8 space-y-4">
           {images.map((image, index) => (
             <img
               key={index}
               src={image}
               alt={`Image ${index}`}
-              className="h-[200px] w-auto object-cover rounded-2xl"
+              className="h-[200px] w-[auto] object-cover rounded-2xl"
             />
           ))}
         </div>
@@ -43,7 +36,7 @@ export function Image() {
       )}
     </div>
   );
-}
+};
 
 const images = [
   "/img1.jpeg",
@@ -57,4 +50,4 @@ const images = [
   "/prj4.png",
 ];
 
-export default Image;
+export default ImagesPage;
